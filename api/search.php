181d26c388852b45e27bfd839455904594c383fc
Tyler Filla <tylerfilla@hotmail.com>
2017-11-04 20:19:48 -0500

    added backend support for keyword search

--- api/search.php
+++ api/search.php
@@ -33,30 +33,48 @@ if ($p_keywords) {
 }
 
 // Get ALL stored math problems
-// The problems are stored in a linked list fashion, so this is unfortunately a necessity
-// The volume of problems in this project is low, however, so it works for now
 $result = $sql->query("SELECT `pid`, `follows` FROM `problem`");
 if (!$result) {
     die("{\"success\": false, \"error\": \"Unable to retrieve problems\"}");
 }
 
-// A list of problem IDs indexed by problem IDs the associated problems follow
-// This allows for easy chained lookups of math problems
-$pids_raw = array();
+// An array of IDs of matched problems
+$matched_pids = array();
 
-// Load above array from database
-// One possible optimization would be to stop after the page ends
-while ($row = $result->fetch_assoc()) {
-    $pids_raw[$row["follows"]] = $row["pid"];
-}
+// A map from IDs of matched problems to the matched keywords
+$matched_keywords = array();
 
-// A list of problem IDs ordered according to user preference
-$pids_ordered = array();
+// Query for keywords and collect matches
+foreach ($keywords as $keyword) {
+    if ($sql_stmt = $sql->prepare("SELECT `pid` FROM `keyword` WHERE `word` = ?")) {
+        // Set up parameter bindings
+        $sql_stmt->bind_param("s", $b_keyword);
 
-// Untangle the linked list structure of the $pids_raw array
-$pid = 0;
-while ($pid = $pids_raw[$pid]) {
-    $pids_ordered[] = $pid;
+        // Execute the query
+        $b_keyword = $keyword;
+        if (!$sql_stmt->execute()) {
+            die("{\"success\": false, \"error\": \"Unable to query keyword: $sql->error\"}");
+        }
+
+        // Process query result
+        $result = $sql_stmt->get_result();
+        while ($row = $result->fetch_assoc()) {
+            $pid = $row["pid"];
+
+            // Store the problem ID if it doesn't already exist
+            if (!in_array($pid, $matched_pids)) {
+                $matched_pids[] = $pid;
+            }
+
+            // Use the problem ID as an index to store the keyword
+            if (!$matched_keywords[$pid]) {
+                $matched_keywords[$pid] = array();
+            }
+            $matched_keywords[$pid][] = $keyword;
+        }
+    } else {
+        die("{\"success\": false, \"error\": \"Unable to prepare to query keyword: $sql->error\"}");
+    }
 }
 
 $success_result = "{\"success\": true, \"result\": {\"keywords\": [";
@@ -72,27 +90,28 @@ for ($i = 0; $i < count($keywords); ++$i) {
 
 $success_result .= "], \"problems\": [";
 
-// List all queried problems
-for ($i = 0; $i < count($pids_ordered); ++$i) {
+// List all matched problems
+for ($i = 0; $i < count($matched_pids); ++$i) {
     if ($i > 0) {
         $success_result .= ",";
     }
 
-    $pid = $pids_ordered[$i];
-    $content = null;
+    $m_pid = $matched_pids[$i];
+    $m_keywords = $matched_keywords[$m_pid];
+    $m_content = null;
 
     if ($sql_stmt = $sql->prepare("SELECT `content` FROM `problem` WHERE `pid` = ?")) {
         // Set up parameter bindings
         $sql_stmt->bind_param("i", $b_pid);
 
         // Execute the query
-        $b_pid = $pid;
+        $b_pid = $m_pid;
         if (!$sql_stmt->execute()) {
             die("{\"success\": false, \"error\": \"Unable to get content: $sql->error\"}");
         }
 
         // Fetch content
-        $content = $sql_stmt->get_result()->fetch_assoc()["content"];
+        $m_content = $sql_stmt->get_result()->fetch_assoc()["content"];
 
         // Close the statement
         $sql_stmt->close();
@@ -102,9 +121,20 @@ for ($i = 0; $i < count($pids_ordered); ++$i) {
 
     // Base64-encode the content, because there are just too many things to try to escape
     // We just assume that all the character-level stuff (i.e. encoding) will work out
-    $content_b64 = base64_encode($content);
+    $content_b64 = base64_encode($m_content);
 
-    $success_result .= "{\"pid\": $pid, \"content\": \"$content_b64\"}";
+    $success_result .= "{\"pid\": $m_pid, \"num_keywords\": " . count($m_keywords) . ", \"keywords\": [";
+
+    // List matching keywords
+    for ($j = 0; $j < count($m_keywords); ++$j) {
+        if ($j > 0) {
+            $success_result .= ",";
+        }
+
+        $success_result .= "\"" . addslashes($m_keywords[$j]) . "\"";
+    }
+
+    $success_result .= "], \"content\": \"$content_b64\"}";
 }
 
 $success_result .= "]}}";
